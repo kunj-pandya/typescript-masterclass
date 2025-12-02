92a128aafb879da8a4758c5128564d306986a762
kunj-pandya <kunjpandya998@gmail.com>
2025-12-02 20:47:33 +0530

    remove code files

--- day01-intro/intro.ts
+++
@@ -1,8 +0,0 @@
-// TypeScript basic type inference example
-
-let user = { name: "kunj", age: 24 }
-
-console.log("kunj");
-console.log(user.name);
-
-// console.log(user.email); // shows error if not defined
