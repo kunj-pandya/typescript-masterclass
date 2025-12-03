1c488855ea2a782205aa847265488dfff2f03baf
kunj-pandya <kunjpandya998@gmail.com>
2025-12-03 17:03:16 +0530

    add primitive types with basic syntex and example

---
+++ 02-types-and-inference/01-primitive-types.ts
@@ -0,0 +1,24 @@
+//  --> Primtive Types : string, number, boolean, null, undefined.
+
+// -> syntex : let veriableName: type = value;
+
+
+//  -> string : Represents string values.
+let userName: string = "kunj";
+
+
+// -> number : Represents integers, floats, decimals.
+let age: number = 24;
+
+//Note : JavaScript does not have a special runtime value for integers, so there’s no equivalent to int or float - everything is simply number
+
+// -> boolean : Represents true/false values.
+let isStudent: boolean = true;
+
+// -> null: Represents absence of value.
+let data: null = null;
+
+// -> undefined : Represents a variable that is declared but not assigned.
+let score: undefined = undefined;
+
+export { }
