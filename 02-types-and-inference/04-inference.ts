d4a333625df404a5813a8b2a8b6deaae2bf12db9
kunj-pandya <kunjpandya998@gmail.com>
2026-01-19 13:25:10 +0530

    remove type and interface folder

--- 02-types-and-inference/04-inference.ts
+++
@@ -1,7 +0,0 @@
-// --> Type Annotations on Variables : When you declare a variable using const, var, or let, you can optionally add a type annotation to explicitly specify the type of the variable:
-
-// let myName: string = "kunj";
-
-
-// No type annotation needed -- 'myName' inferred as type 'string'
-let myName = "kunj";
